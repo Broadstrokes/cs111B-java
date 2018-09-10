c24dbea7aac0e3df5b841fa142fa75579f4309da
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-09 22:38:58 -0700

    Java - Print Asterisk

--- Asterisks.java
+++ Asterisks.java
@@ -1,23 +1,60 @@
+// import java.util.Scanner;
+
+// public class Asterisks {
+//     public static void main (String args[]) {
+//         int count;
+//         char goAgain = 'y';
+//         Scanner input = new Scanner(System.in);
+
+//         while(goAgain != 'n') {
+//             System.out.print("How many asterisks?: ");
+            
+//             count = input.nextInt();
+            
+//             for (int i = 0; i < count; i++) {
+//                 System.out.print("*");
+//             }
+
+//             System.out.println();
+            
+//             System.out.print("Go again? (y/n): ");
+//             goAgain = input.next().charAt(0);
+//         }
+//     }
+// }
+
+
+
 import java.util.Scanner;
 
-public class Asterisks {
-    public static void main (String args[]) {
+
+public class Asterisks{
+    private static int randNumber() {
+        int min = 1;
+        int max = 10;
+
+        
+    }
+
+
+    public static void main(String args[]) {
         int count;
         char goAgain = 'y';
         Scanner input = new Scanner(System.in);
 
-        while(goAgain != 'n') {
-            System.out.print("How many asterisks?: ");
-            
+
+
+        while (goAgain != 'n') {
+            System.out.print("How many? ");
             count = input.nextInt();
-            
-            for (int i = 0; i < count; i++) {
-                System.out.print("*");
+
+            for(int i = 0; i < count; i++) {
+                System.out.print('*');
             }
 
             System.out.println();
-            
-            System.out.print("Go again? (y/n): ");
+
+            System.out.print("go gain");
             goAgain = input.next().charAt(0);
         }
     }
