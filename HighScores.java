4f32fbeb5f909e6c4513bb6345ab80f0dea7f172
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-25 19:52:17 -0700

    Beautify code

--- HighScores.java
+++ HighScores.java
@@ -5,94 +5,79 @@ import java.util.Collections;
 public class HighScores {
 
 
-	public static void main(String[] args) {
-		ArrayList<String> names = new ArrayList<>(5);
-		ArrayList<Integer> scores = new ArrayList<>(5);
+ public static void main(String[] args) {
+  ArrayList < String > names = new ArrayList < > (5);
+  ArrayList < Integer > scores = new ArrayList < > (5);
 
-		initialize(names, scores);
-		sort(names, scores);
-		display(names, scores);
-	}
+  initialize(names, scores);
+  sort(names, scores);
+  display(names, scores);
+ }
 
 
-	public static void initialize(ArrayList<String> names, ArrayList<Integer> scores) {
-	    Scanner input = new Scanner(System.in);
-	    int size = 5;
-			int count = 0;
-	    final String PROMPT1 = "Enter the name for score #";
-	    final String PROMPT2 = "Enter the score for score #";
+ public static void initialize(ArrayList < String > names, ArrayList < Integer > scores) {
+  Scanner input = new Scanner(System.in);
+  int size = 5;
+  int count = 0;
+  final String PROMPT1 = "Enter the name for score #";
+  final String PROMPT2 = "Enter the score for score #";
 
-			while(count < size) {
-				count++;
-				
-				print(PROMPT1 + count + ": ");
-				names.add(input.next());
-				print(PROMPT2 + count + ": ");
-				scores.add(input.nextInt());
-			}	
-	}
+  while (count < size) {
+   count++;
 
-	public static void sort(ArrayList<String> names, ArrayList<Integer> scores) {
-		int indexToSwap;
-		int size = scores.size();
+   print(PROMPT1 + count + ": ");
+   names.add(input.next());
+   print(PROMPT2 + count + ": ");
+   scores.add(input.nextInt());
+  }
+ }
 
-		for (int i = 0; i < size; i++) {
-		    indexToSwap = findIndexOfLargest(scores, i, size);
-		    Collections.swap(scores, i, indexToSwap);
-		    Collections.swap(names, i, indexToSwap);
-		}
-	}
+ public static void sort(ArrayList < String > names, ArrayList < Integer > scores) {
+  int indexToSwap;
+  int size = scores.size();
 
-	public static void display(ArrayList<String> names, ArrayList<Integer> scores) {
-		int count = 0;
-		int size = scores.size();
-	
-		println("Top Scorers: ");
+  for (int i = 0; i < size; i++) {
+   indexToSwap = findIndexOfLargest(scores, i, size);
+   Collections.swap(scores, i, indexToSwap);
+   Collections.swap(names, i, indexToSwap);
+  }
+ }
 
-		while (count < size) {
-		    println(names.get(count) + ": " + scores.get(count));
-		    count++;
-		}
-	}
+ public static void display(ArrayList < String > names, ArrayList < Integer > scores) {
+  int count = 0;
+  int size = scores.size();
 
-		////////////////////
-		// HELPER METHODS //
-		////////////////////
+  println("Top Scorers: ");
 
-	/*
-		Helper method for sort
-	 */
-	public static int findIndexOfLargest(ArrayList<Integer> scores, int startingIndex, int numItems) {
-	    int indexOfLargest = startingIndex;
-	    
-	    for (int i = startingIndex + 1; i < numItems; i++) {
-	        if (scores.get(i) > scores.get(indexOfLargest)) {
-	            indexOfLargest = i;
-	        }
-	    }
-	    
-	    return indexOfLargest;
-	}
+  while (count < size) {
+   println(names.get(count) + ": " + scores.get(count));
+   count++;
+  }
+ }
 
-	public static void println(Object line) { System.out.println(line); }
-	public static void print(Object line) { System.out.print(line); }
+ ////////////////////
+ // HELPER METHODS //
+ ////////////////////
+
+ /*
+ 	Helper method for sort
+  */
+ public static int findIndexOfLargest(ArrayList < Integer > scores, int startingIndex, int numItems) {
+  int indexOfLargest = startingIndex;
+
+  for (int i = startingIndex + 1; i < numItems; i++) {
+   if (scores.get(i) > scores.get(indexOfLargest)) {
+    indexOfLargest = i;
+   }
+  }
+
+  return indexOfLargest;
+ }
+
+ public static void println(Object line) {
+  System.out.println(line);
+ }
+ public static void print(Object line) {
+  System.out.print(line);
+ }
 }
-
-
-
-
-/*
-All of the user input should be done in a method named initialize. It should have the following signature:
-
-public static void initialize(ArrayList<String> names, ArrayList<Integer> scores)
-
-You should write a function that sorts both array lists, based on the values in the scores array list. This is one of the more conceptually challenging parts of the assignment. You want to sort the scores array list, and simultaneously alter the names array list so that the names continue to line up with their respective scores by index. In the example data above, when the score 9900 moves to the first element of the scores array list, the name “Kim” should also be moved to the top of the names array list. The function should have the following signature:
-
-public static void sort(ArrayList<String> names, ArrayList<Integer> scores)
-
-Finally you should write a method that displays the contents of the two arraylists. It should have the following signature:
-
-public static void display(ArrayList<String> names, ArrayList<Integer> scores)
-
-The main method should be very short. It should just declare and initialize the two arraylists and then invoke these three methods.
-*/
