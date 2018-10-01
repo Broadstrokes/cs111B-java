87d90aa8a1285b8a8f190e71061b96394cb0e5a4
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-30 21:09:40 -0700

    Create MyCircle class skeleton

--- MyCircle.java
+++ MyCircle.java
@@ -1,3 +1,22 @@
 /*
 	MyCircle Class
 */
+
+
+public class MyCircle {
+	// FIELDS
+	private double x,
+	private double y,
+	private double radius;
+
+	//METHODS
+	//Setters
+	public void setX(double value) {}
+	public void setY(double value) {}
+	public void setRadius(double value) {}
+
+	//Getters
+	public double getX() {}
+	public double getY() {}
+	public double getRadius() {}
+}
