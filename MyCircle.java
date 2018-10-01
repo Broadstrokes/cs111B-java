fd641f20cd17f0b5d242691933d2e9d8cda2367a
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-30 21:10:21 -0700

    Add comments for setters

--- MyCircle.java
+++ MyCircle.java
@@ -4,15 +4,31 @@
 
 
 public class MyCircle {
-	// FIELDS
 	private double x,
 	private double y,
 	private double radius;
 
-	//METHODS
-	//Setters
-	public void setX(double value) {}
+	/**
+	 * The setX method stores a value in the
+	 * x field
+	 * @param value The value to store in x
+	 */
+	public void setX(double value) {
+		x = value;
+	}
+
+	/**
+	 * The setY method stores a value in the 
+	 * y field
+	 * @param value The value to store in y
+	 */
 	public void setY(double value) {}
+
+	/**
+	 * The setRadius method stores a value in the
+	 * radius field
+	 * @param value The value to store in radius
+	 */
 	public void setRadius(double value) {}
 
 	//Getters
