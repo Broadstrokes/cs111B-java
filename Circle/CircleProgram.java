e33ecad286dae2a2756f1fddd7b3621524b8f50e
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-14 15:08:22 -0700

    Organize tests for Circle class

--- Circle/CircleProgram.java
+++ Circle/CircleProgram.java
@@ -47,8 +47,9 @@ public class CircleProgram {
 		test((testCircle4.getArea() == Math.PI * 5 * 5), "Passed", "Failed", "should get area");
 		System.out.println(testCircle4.toString());
 
-
+		System.out.println("=====================================");
 		System.out.println("======TESTING GETTERS & SETTERS======");
+		System.out.println("=====================================");
 
 		testCircle1.setX(5);
 		testCircle1.setY(10);
@@ -59,8 +60,9 @@ public class CircleProgram {
 		test((testCircle1.getY() == 10), "Passed", "Failed", "should get updated y coordiante");
 		test((testCircle1.getRadius() != 0), "Passed", "Failed", "should get new radius");
 
-
+		System.out.println("===================================================");
 		System.out.println("======TESTING IF 2 Circles have same x, y, r ======");
+		System.out.println("===================================================");
 		Circle testCircle5 = new Circle(4, 5, 10);
 		Circle testCircle6 = new Circle(4, 5, 10);
 		Circle testCircle7 = new Circle(4, 3, 10);
@@ -68,8 +70,9 @@ public class CircleProgram {
 		test((testCircle5.equals(testCircle6)), "Passed", "Failed", "should indicate testCircle5 equals testCircle6");
 		test((testCircle5.equals(testCircle7) == false), "Passed", "Failed", "should indicate testCircle5 doesn't equal testCircle7");
 
-
-		System.out.println("======TESTING OVERLAP======");
+		System.out.println("================================");
+		System.out.println("======TESTING OVERLAP===========");
+		System.out.println("================================");
 		test((a.doesOverlap(b) == true), "Passed", "Failed", "should indicate Circle a overlaps Circle b");
 		test((a.doesOverlap(c) == true), "Passed", "Failed", "should indicate Circle a overlaps Circle c");
 		test((b.doesOverlap(a) == true), "Passed", "Failed", "should indicate Circle b overlaps Circle a");
@@ -77,7 +80,9 @@ public class CircleProgram {
 		test((d.doesOverlap(b) == true), "Passed", "Failed", "should indicate Circle d overlaps Circle b");
 		test((b.doesOverlap(d) == true), "Passed", "Failed", "should indicate Circle b overlaps Circle d");
 		
-		System.out.println("======TESTING NO OVERLAP======");
+		System.out.println("================================");
+		System.out.println("======TESTING NO OVERLAP========");
+		System.out.println("================================");
 		test((a.doesOverlap(d) == false), "Passed", "Failed", "should indicate Circle a doesn't overlap Circle d");
 		test((b.doesOverlap(c) == false), "Passed", "Failed", "should indicate Circle b doesn't overlap Circle c");
 		test((c.doesOverlap(b) == false), "Passed", "Failed", "should indicate Circle c doesn't overlap Circle b");
@@ -93,11 +98,13 @@ public class CircleProgram {
 		myCircles.add(c); // overlaps with A
 		myCircles.add(d); // overlaps with B
 
-
-		System.out.println("======RETURN AREA OF EACH CIRCLE======");
+		System.out.println("============================================");
+		System.out.println("======CALCULATE AREA OF A GIVEN CIRCLE======");
+		System.out.println("============================================");
 
 		for (Circle circle : myCircles) {
-			System.out.println(circle.getArea());
+			// toString utilizes the getArea method
+			System.out.println(circle.toString());
 		}
 	}
 
