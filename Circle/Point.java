16b58f7ae47c0aa0e0f53993b447b4273d323f27
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-14 12:49:30 -0700

    Add method to Point class to calculate distance b/w 2 points

--- Circle/Point.java
+++ Circle/Point.java
@@ -37,4 +37,12 @@ public class Point {
  public String toString() {
   return "(" + x + ", " + y + ")";
  }
+
+ public double distance(Point otherPoint) {
+ 	return Math.sqrt(
+ 		Math.pow((x - otherPoint.getX()), 2) +
+ 		Math.pow((y - otherPoint.getY()), 2)
+ 	);
+ }
+ 
 }
