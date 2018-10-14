eb801c73143426e75dcf3a9d1ef44eb40afb6089
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-14 15:48:38 -0700

    Fix typo

--- Circle/CircleProgram.java
+++ Circle/CircleProgram.java
@@ -16,24 +16,24 @@ public class CircleProgram {
 		System.out.println("====== CONSTRUCTOR w/ no-args ======");
 		Circle testCircle1 = new Circle();
 		
-		test((testCircle1.getX() == 0), "Passed", "Failed", "should get x coordiante");
-		test((testCircle1.getY() == 0), "Passed", "Failed", "should get y coordiante");
+		test((testCircle1.getX() == 0), "Passed", "Failed", "should get x coordinate");
+		test((testCircle1.getY() == 0), "Passed", "Failed", "should get y coordinate");
 		test((testCircle1.getRadius() == 0), "Passed", "Failed", "should get radius");
 		System.out.println(testCircle1.toString());
 
 		System.out.println("====== CONSTRUCTOR w/ Point ======");
 		Point p = new Point(3, 2);
 		Circle testCircle2 = new Circle(p, 10);
-		test((testCircle2.getX() == 3), "Passed", "Failed", "should get x coordiante");
-		test((testCircle2.getY() == 2), "Passed", "Failed", "should get y coordiante");
+		test((testCircle2.getX() == 3), "Passed", "Failed", "should get x coordinate");
+		test((testCircle2.getY() == 2), "Passed", "Failed", "should get y coordinate");
 		test((testCircle2.getRadius() == 10), "Passed", "Failed", "should get radius");
 		test((testCircle2.getArea() == Math.PI * 10 * 10), "Passed", "Failed", "should get area");
 		System.out.println(testCircle2.toString());
 
 		System.out.println("====== CONSTRUCTOR w/ x, y, & r ======");
 		Circle testCircle3 = new Circle(3, 2, 10);
-		test((testCircle3.getX() == 3), "Passed", "Failed", "should get x coordiante");
-		test((testCircle3.getY() == 2), "Passed", "Failed", "should get y coordiante");
+		test((testCircle3.getX() == 3), "Passed", "Failed", "should get x coordinate");
+		test((testCircle3.getY() == 2), "Passed", "Failed", "should get y coordinate");
 		test((testCircle3.getRadius() == 10), "Passed", "Failed", "should get radius");
 		test((testCircle3.getArea() == Math.PI * 10 * 10), "Passed", "Failed", "should get area");
 		System.out.println(testCircle3.toString());
@@ -41,8 +41,8 @@ public class CircleProgram {
 
 		System.out.println("====== CONSTRUCTOR w/ Circle Object ======");
 		Circle testCircle4 = new Circle(b);
-		test((testCircle4.getX() == -6), "Passed", "Failed", "should get x coordiante");
-		test((testCircle4.getY() == 4), "Passed", "Failed", "should get y coordiante");
+		test((testCircle4.getX() == -6), "Passed", "Failed", "should get x coordinate");
+		test((testCircle4.getY() == 4), "Passed", "Failed", "should get y coordinate");
 		test((testCircle4.getRadius() == 5), "Passed", "Failed", "should get radius");
 		test((testCircle4.getArea() == Math.PI * 5 * 5), "Passed", "Failed", "should get area");
 		System.out.println(testCircle4.toString());
@@ -56,8 +56,8 @@ public class CircleProgram {
 		testCircle1.setRadius(10);
 
 
-		test((testCircle1.getX() == 5), "Passed", "Failed", "should get updated x coordiante");
-		test((testCircle1.getY() == 10), "Passed", "Failed", "should get updated y coordiante");
+		test((testCircle1.getX() == 5), "Passed", "Failed", "should get updated x coordinate");
+		test((testCircle1.getY() == 10), "Passed", "Failed", "should get updated y coordinate");
 		test((testCircle1.getRadius() != 0), "Passed", "Failed", "should get new radius");
 
 		System.out.println("===================================================");
@@ -112,8 +112,9 @@ public class CircleProgram {
 	/**
 	 * Function to test an expectation
 	 * @param expression     Expectation/Expression to test, which returns a boolean
-	 * @param successMessage What to output to the user if the expectation is met
-	 * @param errorMessage   What to output to the user if the expectation is not met
+	 * @param successMessage String to output to the user if the expectation is met
+	 * @param errorMessage   String to output to the user if the expectation is not met
+	 * @param should         String stating what should be tested
 	 */
 	public static void test (boolean expression, String successMessage, String errorMessage, String should) {
 		System.out.print(should + ": ");
