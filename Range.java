a41e46f3d66040a23cdd20dd56a195e6800f2e0c
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-20 22:26:33 -0700

    Create Range class

---
+++ Range.java
@@ -0,0 +1,39 @@
+ public class Range {
+	private 
+		int lowerBound;
+		int upperBound;
+
+
+  Range(int lower, int upper) {
+		lowerBound = lower;
+		upperBound = upper;
+  }
+
+  void setLowerBound(int value) {
+		lowerBound = value;
+  }
+
+  int getLowerBound() {
+		return lowerBound;
+  }
+
+  void setUpperBound(int value) {
+		upperBound = value;
+  }
+
+
+  int getUpperBound() {
+		return upperBound;
+  }
+
+  boolean contains(int value) {
+		return (lowerBound <= value) && (value <= upperBound); 
+  }
+
+  boolean equals(Range r) {
+		return (
+			(lowerBound == r.lowerBound) && 
+			(upperBound == r.upperBound)
+		);
+  }
+ }	
