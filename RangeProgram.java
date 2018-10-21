fb3ff22f22fc587fb8ceb0cdce8061b9e9c01253
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-20 22:42:43 -0700

    Update range driver

--- RangeProgram.java
+++ RangeProgram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public RangeProgram() {
+public class RangeProgram {
 	public static void main(String[] args) {
 		
 		// Create two range objects
@@ -9,15 +9,20 @@ public RangeProgram() {
 
 		// Read an integer
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a value: ");
+		System.out.print("Enter a value: ");
 		int input = in.nextInt();
 
 		// Check to see if the integer is in r1
 		if (r1.contains(input))    
 			System.out.println("r1 contains " + input);
+		else
+			System.out.println("r1 does not contains " + input);
+
 
 		// Check to see if r2 is fully inside r1
 		if (r1.contains(r2)) 
-		    System.out println("r1 contains r2");		 
+		    System.out.println("r1 contains r2");		 
+		else
+			System.out.println("Range 2 is not inside range 1");
 	}
 }
