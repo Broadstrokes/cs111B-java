2705eebd9a0960548a850d43e6beb5d4d630881b
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-20 22:57:54 -0700

    Add more test cases to the RangeProgram

--- RangeProgram.java
+++ RangeProgram.java
@@ -9,20 +9,75 @@ public class RangeProgram {
 
 		// Read an integer
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter a value: ");
+		print("Enter a value: ");
 		int input = in.nextInt();
 
 		// Check to see if the integer is in r1
 		if (r1.contains(input))    
-			System.out.println("r1 contains " + input);
+			println("r1 contains " + input);
 		else
-			System.out.println("r1 does not contains " + input);
+			println("r1 does not contain " + input);
 
 
 		// Check to see if r2 is fully inside r1
 		if (r1.contains(r2)) 
-		    System.out.println("r1 contains r2");
+			println("r1 contains r2");
 		else
-			System.out.println("Range 2 is not inside range 1");
+			println("Range 2 is not inside range 1");
+
+		println("==========================");
+		println(" TEST CONSTRUCTOR ");
+		println("==========================");
+		test(r1.getUpperBound() == 100, "passed", "failed", "set correct upper bound");
+		test(r1.getLowerBound() == 1, "passed", "failed", "set correct lower bound");
+		test(r2.getUpperBound() == 30, "passed", "failed", "set correct upper bound");
+		test(r2.getLowerBound() == 20, "passed", "failed", "set correct lower bound");
+
+
+		println("==========================");
+		println(" TEST GETTERS & SETTERS ");
+		println("==========================");
+
+		r1.setUpperBound(15);
+		test(r1.getUpperBound() == 15, "passed", "failed", "set correct upper bound");
+		r1.setLowerBound(10);
+		test(r1.getLowerBound() == 10, "passed", "failed", "set correct lower bound");
+
+
+		// Check to see if the integer is in r1
+		if (r1.contains(input))    
+			println("r1 contains " + input);
+		else
+			println("r1 does not contain " + input);
+
+
+		// Check to see if r2 is fully inside r1
+		if (r1.contains(r2)) 
+			println("r1 contains r2");
+		else
+			println("Range 2 is not inside range 1");
+
+
 	}
+
+
+  public static void println(Object line) { System.out.println(line); }
+  public static void print(Object line) { System.out.print(line); }
+
+  /**
+   * Function to test an expectation
+   * @param expression     Expectation/Expression to test, which returns a boolean
+   * @param successMessage String to output to the user if the expectation is met
+   * @param errorMessage   String to output to the user if the expectation is not met
+   * @param should         String stating what should be tested
+   */
+  public static void test (boolean expression, String successMessage, String errorMessage, String should) {
+  	System.out.print(should + ": ");
+
+  	if (expression)
+  		System.out.println(successMessage);
+  	else
+  		System.out.println(errorMessage);
+  }
+
 }
