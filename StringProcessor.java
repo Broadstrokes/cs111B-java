2a3c5414c90026954d703a3970adead1181bd188
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-05 18:43:12 -0800

    Add constructor, setString, getString, wordCount methods

--- StringProcessor.java
+++ StringProcessor.java
@@ -1,3 +1,50 @@
 public class StringProcessor {
+	private String str; 
 
+	public StringProcessor() { 
+		str = "";
+	}
+
+	public StringProcessor(String s) { 
+		str = s;
+	}
+
+
+	public void setString(String s) { 
+		str = s;
+	}
+
+	public String getString() { 
+		return this.str;
+	}
+
+
+	public int wordCount() { 
+		return this.str.length();
+	}
+
+	public int uppercaseCount() { 
+
+	}
+
+	// public int digitCount() { 
+
+	// }
+
+	// public int digitWordCount() { 
+
+	// }
+
+
+	// public String getNoSpaceString() { 
+
+	// }
+
+	// public String getNoVowelString() { 
+
+	// }
+
+	// public String getNoDigitWordString() { 
+
+	// }
 }
