67364d0ffb8c18d5a596481372f327d9a6d97872
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-06 00:38:47 -0800

    Prettify code

--- StringProcessor.java
+++ StringProcessor.java
@@ -1,133 +1,155 @@
 public class StringProcessor {
-	final private char[] vowelsArray = new char[] {'a', 'e', 'i', 'o', 'u'};
-	final private String digitWords = new String(
-		"zero one two three four five six seven eight nine"
-	);
-	final private String[] digits = new String[] {"0", "1", "2", "3", "4", "6", "7", "8", "9"};
-	private String str;
-
-	public StringProcessor() { 
-		str = "";
-	}
-
-	public StringProcessor(String s) { 
-		str = s;
-	}
+ final private String digitWords = new String(
+  "zero one two three four five six seven eight nine"
+ );
+ final private String[] digits = new String[] { "0", "1", "2", "3", "4", "6", "7", "8", "9" };
+ private String str;
 
 
-	public void setString(String s) { 
-		str = s;
-	}
-
-	public String getString() { 
-		return this.str;
-	}
+ public StringProcessor() {
+  str = "";
+ }
 
 
-	public int wordCount() { 
-		return this.str.split(" ").length;
-	}
+ public StringProcessor(String s) {
+  str = s;
+ }
 
-	public int uppercaseCount() { 
-		int count = 0;
-		for (int i = 0; i < this.str.length(); i++) {
-			if(Character.isUpperCase(str.charAt(i))) {
-				count++;
-			}
+
+ public void setString(String s) {
+  str = s;
+ }
+
+
+ public String getString() {
+  return this.str;
+ }
+
+
+ public int wordCount() {
+  return this.str.split(" ").length;
+ }
+
+
+ public int uppercaseCount() {
+  int count = 0;
+  for (int i = 0; i < this.str.length(); i++) {
+   if (Character.isUpperCase(str.charAt(i))) {
+    count++;
+   }
+  }
+  return count;
+ }
+
+ public int digitCount() {
+  int count = 0;
+  for (int i = 0; i < this.str.length(); i++) {
+   if (Character.isDigit(str.charAt(i))) {
+    count++;
+   }
+  }
+  
+  return count;
+ }
+
+
+ /**
+  * Returns the count of words that are digits
+  */
+ public int digitWordCount() {
+  int count = 0;
+  String currentWord;
+  String currentWordWithoutSpecialCharacters;
+	// break apart the user's input string into an array
+  String[] userInputTokenized = this.str.split(" ");
+
+  for (int i = 0; i < userInputTokenized.length; i++) {
+		currentWord = new String(userInputTokenized[i]);
+		// remove any special characters
+		currentWordWithoutSpecialCharacters = currentWord.replaceAll("[^\\w\\s]", "");
+		// checks if the current word from the user's input matches a word in the
+		// digitsWords string, which holds all the digits ("zero one two three ... nine")
+		if (digitWords.indexOf(currentWordWithoutSpecialCharacters.toLowerCase()) > 1) {
+		count++;
 		}
-		return count;
-	}
+  }
 
-	public int digitCount() { 
-		int count = 0;
-		for (int i = 0; i < this.str.length(); i++) {
-			if (Character.isDigit(str.charAt(i))) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	public int digitWordCount() { 
-		int count = 0;
-		String currentWord;
-		String currentWordWithoutSpecialCharacters;
-		String[] tokens = this.str.split(" ");
-
-		for(int i = 0; i < tokens.length; i++) {
-			currentWord = new String(tokens[i]);
-			currentWordWithoutSpecialCharacters = currentWord.replaceAll("[^\\w\\s]","");
-
-			if (digitWords.indexOf(currentWordWithoutSpecialCharacters.toLowerCase()) > 1) {
-				count++;
-			}			
-		}
-
-		return count;
-	}
-
-	public String getNoSpaceString() {
-		char ch;
-		StringBuilder myNewStr = new StringBuilder("");
-
-		for (int i = 0; i < this.str.length(); i++) {
-			ch = this.str.charAt(i);
-			if (!Character.isWhitespace(ch)) {
-				myNewStr.append(ch);
-			}
-		}	
-			return myNewStr.toString();
-	}
+  return count;
+ }
 
 
-	public String getNoVowelString() { 
-		char currentCharacterLowercased;
-		StringBuilder myNewStr = new StringBuilder("");
+ /**
+  * Returns a new string with all whitespaces removed
+  */
+ public String getNoSpaceString() {
+  char ch;
+  StringBuilder myNewStr = new StringBuilder("");
 
-		for (int i = 0; i < this.str.length(); i++) {
-			currentCharacterLowercased = Character.toLowerCase(this.str.charAt(i));
+  for (int i = 0; i < this.str.length(); i++) {
+   ch = this.str.charAt(i);
+   if (!Character.isWhitespace(ch)) {
+    myNewStr.append(ch);
+   }
+  }
 
-			if (
-					currentCharacterLowercased == 'a' ||
-					currentCharacterLowercased == 'e' ||
-					currentCharacterLowercased == 'i' ||
-					currentCharacterLowercased == 'o' ||
-					currentCharacterLowercased == 'u'
-				) {
-				myNewStr.append('-');
-			} else {
-				myNewStr.append(this.str.charAt(i));
-				
-			}
-		}
-		
-		return myNewStr.toString();
-	}
+  return myNewStr.toString();
+ }
 
-	public String getNoDigitWordString() {
-		String[] digitWordsArray = this.digitWords.split(" ");
-		String[] tokens = this.str.split(" "); // convert the string into array
-		int startingIndexOfWordDigitIfFoundInString;
-		String currentDigit;
 
-		// Loop over the tokenized user input string
-		for (int i = 0; i < tokens.length; i++) {
-			// Loop over the word digits array called digitWordsArray {"zero", "one", ... "nine"}
-			for (int j = 0; j < digitWordsArray.length; j++) {
-				currentDigit = digitWordsArray[j];
-				startingIndexOfWordDigitIfFoundInString = tokens[i].toLowerCase().indexOf(digitWordsArray[j]);
+ /**
+  * Returns a new string with vowels replaced by hypens (-)
+  */
+ public String getNoVowelString() {
+  char currentCharacterLowercased;
+  StringBuilder myNewStr = new StringBuilder("");
 
-				// This check ensures that a word such as money doesn't turn into m1y
-				if (startingIndexOfWordDigitIfFoundInString == 0) {
-					// Using a StringBuilder replace the substring with correct digit eg: one get converted to "1"
-					StringBuilder temp = new StringBuilder(tokens[i]);
-					temp.replace(0, digitWordsArray[j].length(), digits[j]);
-					// Place the item in the tokens array
-					tokens[i] = temp.toString();
-				}
-			}
-		}
+  for (int i = 0; i < this.str.length(); i++) {
+   currentCharacterLowercased = Character.toLowerCase(this.str.charAt(i));
 
-		return String.join(" ", tokens);
-	}
+   if (
+    currentCharacterLowercased == 'a' ||
+    currentCharacterLowercased == 'e' ||
+    currentCharacterLowercased == 'i' ||
+    currentCharacterLowercased == 'o' ||
+    currentCharacterLowercased == 'u'
+   ) {
+    myNewStr.append('-');
+   } else {
+    myNewStr.append(this.str.charAt(i));
+
+   }
+  }
+
+  return myNewStr.toString();
+ }
+
+ /**
+  * Returns a new string with all word numbers converted to digits
+  */
+ public String getNoDigitWordString() {
+  String[] digitWordsArray = this.digitWords.split(" ");
+  String[] userInputTokenized = this.str.split(" "); // convert the string into array
+  int startingIndexOfWordDigitIfFoundInString;
+  String currentDigit;
+
+  // Loop over the tokenized user input string
+  for (int i = 0; i < userInputTokenized.length; i++) {
+   // Loop over the word digits array called digitWordsArray {"zero", "one", ... "nine"}
+   for (int j = 0; j < digitWordsArray.length; j++) {
+    currentDigit = digitWordsArray[j];
+    startingIndexOfWordDigitIfFoundInString = userInputTokenized[i].toLowerCase().indexOf(digitWordsArray[j]);
+
+    // This check ensures that a word such as money doesn't turn into m1y
+    if (startingIndexOfWordDigitIfFoundInString == 0) {
+     // Using a StringBuilder replace the substring with correct digit eg: one get converted to "1"
+     StringBuilder temp = new StringBuilder(userInputTokenized[i]);
+     temp.replace(0, digitWordsArray[j].length(), digits[j]);
+     // Place the item in the userInputTokenized array
+     userInputTokenized[i] = temp.toString();
+    }
+   }
+  }
+
+  return String.join(" ", userInputTokenized);
+ }
 }
--- StringProcessorProgram.java
+++ StringProcessorProgram.java
@@ -2,68 +2,75 @@ import java.util.Scanner;
 
 
 public class StringProcessorProgram {
-	public static void main(String[] args) {
-		println("Starting String Processor Program");
+ public static void main(String[] args) {
+  println("Starting String Processor Program");
 
-		Scanner input = new Scanner(System.in);
-		boolean playAgain;
+  Scanner input = new Scanner(System.in);
+  boolean playAgain;
 
-		do {
-			print("Enter a line of text: ");
-			String inputStr = input.nextLine();
-			driver(inputStr);
-			print("do you want to enter another? (y/n): ");
-			playAgain = input.next().charAt(0) == 'y';
-			// Consume the remaining newline character.
-			input.nextLine();
-
-		} while (playAgain);
-
-		
-		/*
-			String inputStr = "One for the money, two for the show";
-			String inputStr2 = "3 Blind Mice is one of my favorite songs.";
-			String inputStr3 = "One for the two, two for the show.";
-
-			driver(inputStr);
-			driver(inputStr2);
-			driver(inputStr3);
-		 */
-	}
+  do {
+		print("Enter a line of text: ");
+		String inputStr = input.nextLine();
+		driver(inputStr);
+		print("do you want to enter another? (y/n): ");
+		playAgain = Character.toLowerCase(input.next().charAt(0)) == 'y';
+		// Consume the remaining newline character.
+		input.nextLine();
+  } while (playAgain);
 
 
+  /*
+  	String inputStr = "One for the money, two for the show";
+  	String inputStr2 = "3 Blind Mice is one of my favorite songs.";
+  	String inputStr3 = "One for the two, two for the show.";
+
+  	driver(inputStr);
+  	driver(inputStr2);
+  	driver(inputStr3);
+   */
+ }
 
 
-	/**
-	 * Function to test an expectation
-	 * @param expression     Expectation/Expression to test, which returns a boolean
-	 * @param successMessage What to output to the user if the expectation is met
-	 * @param errorMessage   What to output to the user if the expectation is not met
-	 */
-	public static void test (boolean expression, String successMessage, String errorMessage) {		
-		if (expression)
-			System.out.println(successMessage);
-		else
-			System.out.println(errorMessage);
-	}
+ public static void driver(String inputStr) {
+  // println(">>>>>>>>>>>>>>>>>>>");
+  // println("TESTING: " + inputStr);
+  // println(">>>>>>>>>>>>>>>>>>>");
 
-  public static void println(Object line) { System.out.println(line); }
-	public static void print(Object line) { System.out.print(line); }
+  StringProcessor stringToBeProcessed = new StringProcessor(inputStr);
 
-	public static void driver(String inputStr) {
-		// println(">>>>>>>>>>>>>>>>>>>");
-		// println("TESTING: " + inputStr);
-		// println(">>>>>>>>>>>>>>>>>>>");
+  println("words: " + (stringToBeProcessed.wordCount()));
+  println("uppercase: " + (stringToBeProcessed.uppercaseCount()));
+  println("digits: " + (stringToBeProcessed.digitCount()));
+  println("digit words: " + (stringToBeProcessed.digitWordCount()));
+  println("line with whitespace removed: " + (stringToBeProcessed.getNoSpaceString()));
+  println("line with vowels replaced: " + (stringToBeProcessed.getNoVowelString()));
+  println("line with digit words replaced: " + (stringToBeProcessed.getNoDigitWordString()));
+ }
 
-		StringProcessor stringToBeProcessed = new StringProcessor(inputStr);
-		
-		println("words: " + (stringToBeProcessed.wordCount()));
-		println("uppercase: " + (stringToBeProcessed.uppercaseCount()));
-		println("digits: " + (stringToBeProcessed.digitCount()));
-		println("digit words: " + (stringToBeProcessed.digitWordCount()));
-		println("line with whitespace removed: " + (stringToBeProcessed.getNoSpaceString()));
-		println("line with vowels replaced: " + (stringToBeProcessed.getNoVowelString()));
-		println("line with digit words replaced: " + (stringToBeProcessed.getNoDigitWordString()));
-	}
+
+ //////////////////////
+ // HELPER FUNCTIONS //
+ //////////////////////
+
+
+ /**
+  * Function to test an expectation
+  * @param expression     Expectation/Expression to test, which returns a boolean
+  * @param successMessage What to output to the user if the expectation is met
+  * @param errorMessage   What to output to the user if the expectation is not met
+  */
+ public static void test(boolean expression, String successMessage, String errorMessage) {
+  if (expression)
+   System.out.println(successMessage);
+  else
+   System.out.println(errorMessage);
+ }
+
+ public static void println(Object line) {
+  System.out.println(line);
+ }
+ public static void print(Object line) {
+  System.out.print(line);
+ }
 
 }
