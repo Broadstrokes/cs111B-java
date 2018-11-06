1fa59ffb7ffb0b66de8c62e459e86911df64077e
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-05 20:24:51 -0800

    Add member method digitWordCount

--- StringProcessor.java
+++ StringProcessor.java
@@ -1,9 +1,9 @@
 public class StringProcessor {
-	private String str; 
-	private String[] digitWords = new String[] {
-		"zero", "one", "two", "three", "four", 
-		"five", "six", "seven", "eight", "nine"
-	};
+	private String str;
+	private char[]  vowelsArray = new char[] {'a', 'e', 'i', 'o', 'u'};
+	private String digitWords = new String(
+		"zero one two three four five six seven eight nine"
+	);
 
 	public StringProcessor() { 
 		str = "";
@@ -47,9 +47,25 @@ public class StringProcessor {
 		return count;
 	}
 
-	// public int digitWordCount() { 
+	public int digitWordCount() { 
+		int count = 0;
+		String currentWord;
+		String cleanCurrentWord;
+		String[] tokens = this.str.split(" ");
 
-	// }
+		for(int i =0; i < tokens.length; i++) {
+			currentWord = new String(tokens[i]);
+			cleanCurrentWord = currentWord.replaceAll("[^\\w\\s]","");
+			// System.out.println(currentWord);
+
+			if(digitWords.indexOf(cleanCurrentWord.toLowerCase()) > 1) {
+				System.out.println(tokens[i]);
+				count++;
+			}			
+		}
+
+		return count;
+	}
 
 
 	// public String getNoSpaceString() { 
