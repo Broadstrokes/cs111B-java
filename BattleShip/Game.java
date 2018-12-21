de2394add5682bafb3917b602b4834deb445a90a
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-21 00:33:30 -0800

    Start the Game class

---
+++ BattleShip/Game.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Game {
+	
+	static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	
+	}
+
+  public static void println(Object line) { System.out.println(line); }
+	public static void print(Object line) { System.out.print(line); }
+
+}
