ce6ff1af4fbc4b224285c3789164378e545d0005
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-21 00:59:23 -0800

    Modify test cases

--- BattleShip/Game.java
+++ BattleShip/Game.java
@@ -6,7 +6,57 @@ public class Game {
 	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-	
+
+		
+
+		testShipMethods();
+
+		// // Add some live conway cells, in a horizontal line		
+		// for (int i = 0; i < 8; i++) {
+		// 	ConwayCell c = new ConwayCell(5, 5 + i, world);
+		// 	c.setIsAlive(true);
+		// 	world.replaceCell(c);
+		// }
+		
+		// // Add an always-alive cells
+		// AbstractCell a = new AlwaysAliveCell(12, 12, world);
+		// world.replaceCell(a);
+			
+		// // Add an always-alive cells
+		// AbstractCell n = new NeverAliveCell(17, 17, world);
+		// world.replaceCell(n);
+
+		// // Add an Blinker cells
+		// BlinkerCell b1 = new BlinkerCell(8, 12, world);
+		// world.replaceCell(b1);
+
+		// BlinkerCell b2 = new BlinkerCell(17, 3, world);
+		// b2.setIsAlive(true);
+		// world.replaceCell(b2);
+
+
+
+		// // Add an Custom cells
+		// for (int i = 0; i < 8; i++) {
+		// 	int randomRow = ThreadLocalRandom.current().nextInt(2, 18);
+		// 	int randomCol = ThreadLocalRandom.current().nextInt(2, 18);
+		// 	CustomCell c1 = new CustomCell(randomRow, randomCol, world);
+		// 	c1.setIsAlive(true);
+		// 	world.replaceCell(c1);
+		// }
+
+			
+		// Go!	
+		// do {
+		// 	board.display();
+		// 	// world.advanceToNextGeneration();
+			
+		// 	System.out.print("Another? (y/n): ");
+		// } while (input.nextLine().charAt(0) == 'y');
+
+	}
+
+	public static void testShipMethods() {
 		// Generate a ship
 		
 		Point p1 = new Point(1,1);
@@ -55,66 +105,38 @@ public class Game {
 		Point p12 = new Point(4,4);
 		Point p13 = new Point(3,2);
 
-		// println(s1.containsPoint(p5)); // f
-		// println(s1.containsPoint(p6)); // t
-		// println(s1.containsPoint(p7)); // t
-		// println(s1.containsPoint(p8)); // t
-		// println(s1.containsPoint(p9)); // f
 
-
-		println(s2.containsPoint(p2)); // t
-		println(s2.containsPoint(p10)); // t
-		println(s2.containsPoint(p11)); // t
-		println(s2.containsPoint(p12)); // f
-		println(s2.containsPoint(p4)); // f
-		
+		test(s1.containsPoint(p5) == false, "Passed", "Failed", (s1.getOrigin().toString() + " & " + s1.getEnd().toString() + " should not contain point " + p2.toString())); // t
+		test(s1.containsPoint(p6) == true, "Passed", "Failed", (s1.getOrigin().toString() + " & " + s1.getEnd().toString() + " should contain point " + p10.toString())); // t
+		test(s1.containsPoint(p7) == true, "Passed", "Failed", (s1.getOrigin().toString() + " & " + s1.getEnd().toString() + " should contain point " + p11.toString())); // t
+		test(s1.containsPoint(p8) == true, "Passed", "Failed", (s1.getOrigin().toString() + " & " + s1.getEnd().toString() + " should contain point " + p12.toString())); // f
+		test(s1.containsPoint(p9) == false, "Passed", "Failed", (s1.getOrigin().toString() + " & " + s1.getEnd().toString() + " should not contain point " + p4.toString())); // f
 
 
 
-
-		// // Add some live conway cells, in a horizontal line		
-		// for (int i = 0; i < 8; i++) {
-		// 	ConwayCell c = new ConwayCell(5, 5 + i, world);
-		// 	c.setIsAlive(true);
-		// 	world.replaceCell(c);
-		// }
-		
-		// // Add an always-alive cells
-		// AbstractCell a = new AlwaysAliveCell(12, 12, world);
-		// world.replaceCell(a);
-			
-		// // Add an always-alive cells
-		// AbstractCell n = new NeverAliveCell(17, 17, world);
-		// world.replaceCell(n);
-
-		// // Add an Blinker cells
-		// BlinkerCell b1 = new BlinkerCell(8, 12, world);
-		// world.replaceCell(b1);
-
-		// BlinkerCell b2 = new BlinkerCell(17, 3, world);
-		// b2.setIsAlive(true);
-		// world.replaceCell(b2);
+		test(s2.containsPoint(p2) == true, "Passed", "Failed", (s2.getOrigin().toString() + " & " + s2.getEnd().toString() + " should contain point " + p2.toString())); // t
+		test(s2.containsPoint(p10) == true, "Passed", "Failed", (s2.getOrigin().toString() + " & " + s2.getEnd().toString() + " should contain point " + p10.toString())); // t
+		test(s2.containsPoint(p11) == true, "Passed", "Failed", (s2.getOrigin().toString() + " & " + s2.getEnd().toString() + " should contain point " + p11.toString())); // t
+		test(s2.containsPoint(p12) == false, "Passed", "Failed", (s2.getOrigin().toString() + " & " + s2.getEnd().toString() + " should not contain point " + p12.toString())); // f
+		test(s2.containsPoint(p4) == false, "Passed", "Failed", (s2.getOrigin().toString() + " & " + s2.getEnd().toString() + " should not contain point " + p4.toString())); // f
+	}
 
 
 
-		// // Add an Custom cells
-		// for (int i = 0; i < 8; i++) {
-		// 	int randomRow = ThreadLocalRandom.current().nextInt(2, 18);
-		// 	int randomCol = ThreadLocalRandom.current().nextInt(2, 18);
-		// 	CustomCell c1 = new CustomCell(randomRow, randomCol, world);
-		// 	c1.setIsAlive(true);
-		// 	world.replaceCell(c1);
-		// }
+	/**
+	 * Function to test an expectation
+	 * @param expression     Expectation/Expression to test, which returns a boolean
+	 * @param successMessage String to output to the user if the expectation is met
+	 * @param errorMessage   String to output to the user if the expectation is not met
+	 * @param should         String stating what should be tested
+	 */
+	public static void test (boolean expression, String successMessage, String errorMessage, String should) {
+		print(should + ": ");
 
-			
-		// Go!	
-		// do {
-		// 	board.display();
-		// 	// world.advanceToNextGeneration();
-			
-		// 	System.out.print("Another? (y/n): ");
-		// } while (input.nextLine().charAt(0) == 'y');
-	
+		if (expression)
+			println(successMessage);
+		else
+			println(errorMessage);
 	}
 
   public static void println(Object line) { System.out.println(line); }
