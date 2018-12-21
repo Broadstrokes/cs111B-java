5d9d2ca66ed9d53126874113ba3e8e003d14596d
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-21 00:39:56 -0800

    Add additional methods to Ship class

--- BattleShip/Ship.java
+++ BattleShip/Ship.java
@@ -32,7 +32,9 @@ public class Ship {
 		// System.out.println("Created ship of length: " + length + " | is vertical? " + isVertical + " starts at " + origin.toString() + " ends at " + end.toString());
 	} 
 
-	// GETTERS
+	/////////////
+	// GETTERS //
+	/////////////
 	public Point getOrigin() { return origin; }
 	public Point getEnd() { return end; }
 	public boolean getIsVertical() { return isVertical; }
@@ -40,8 +42,61 @@ public class Ship {
 
 
 
-	// HELPERS
-	
+
+
+
+	// Returns true if a Ship covers a point on the board, false if it does not.
+	public boolean containsPoint(Point pointToCheck) {
+
+		for (int i = 0; i < listOfPoints.size(); i++) {
+			Point currentPoint = listOfPoints.get(i);
+			if (currentPoint.equals(pointToCheck)) return true;
+		}
+
+		return false;
+	}
+
+	// Returns true if the receiving ship shares a point with the argument ship. Collides with it, so to speak.
+	public boolean collidesWith(Ship s) {
+		Point sOrigin = s.getOrigin();
+		if (s.getIsVertical()) { // check vertically upwards i.e. 'y' axis
+			for (int i = 0; i <= s.getLength(); i++) {
+
+				Point pointToCheck = new Point(sOrigin.getX(), sOrigin.getY() + i);
+				if(this.containsPoint(pointToCheck)) return true;
+			}
+		} else {
+			for (int i = 0; i <= s.getLength(); i++) {
+				Point pointToCheck = new Point(sOrigin.getX() + i, sOrigin.getY());
+				if(this.containsPoint(pointToCheck)) return true;
+			}
+		}
+
+		return false;
+	}
+
+	// //- This is a verb in the game. When the user enters a coordinate, this method can be called on each ship. If the ship contains the point, it should remember that it has been hit at that point. It could do this with a second PointCollection, or some other data strategy.
+	// public void shotFiredAtPoint(Point p) {
+	// 	this.containsPoint(p);
+	// }
+
+	// // - returns true if shotFiredAtPoint has been called for this point in the ship. False if it has not, or if the point is not in the ship.
+	// public boolean isHitAtPoint(Point p) {
+
+	// }
+
+	// //- returns the number of points in the ship that have been hit. When the hitCount is equal to the length of the ship the ship is considered to be sunk.
+	// public int hitCount() {
+
+	// }
+
+
+
+
+	/////////////
+	// HELPERS //
+	/////////////
+
 	private void setEnd(Point origin, boolean isVertical, int length) {
 		if (isVertical) { // add to Y axis
 			end = new Point(origin.getX(), origin.getY() + length);
