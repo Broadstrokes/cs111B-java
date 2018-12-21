61f4a2615b556c517b2f38d116801e3c684473d7
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-21 00:32:41 -0800

    Start the Ship class

---
+++ BattleShip/Ship.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Ship {
+	private Point origin;
+	private Point end;
+	private boolean isVertical;
+	private int length;
+	private ArrayList<Point> listOfPoints = new ArrayList<Point>();
+}
